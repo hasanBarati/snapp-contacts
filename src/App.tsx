5d67ef173c2d02b114e95f2fef24a24c282bf7be
h.barati <hasanbaratiasp@gmail.com>
2024-07-22 09:48:51 +0330

    implement routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link, Route, Router, Routes } from 'react-router-dom';
+import Contacts from './pages/contacts';
+import ContactDetail from './pages/contact-detail';
 
 const App = () => {
-  return <h1>Hello, React with Webpack! {process.env.REACT_APP_API_URL}</h1>;
+  return (
+  
+   <>
+   <h1>Hello, React with Webpack! {process.env.REACT_APP_API_URL}</h1>
+
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Contacts />} />
+        <Route path="/contact" element={<ContactDetail />} />
+      </Routes>
+   
+   </>
+
+  )
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import App from './App';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // import './styles.css'; // if you want to add styles
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode!) ;
+const domNode = document.getElementById("root");
+const root = createRoot(domNode!);
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
