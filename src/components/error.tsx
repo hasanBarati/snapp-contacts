4677b67908ec54f05d93c0f8123ca42885e081b6
hasanb69 <hasanbaratiasp@gmail.com>
2024-07-26 13:35:36 +0330

    Add API Error Handling and Empty State for Lists

---
+++ src/components/error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Error() {
+  return (
+
+      <p className="error">
+        Oops Something went wrong
+      </p>
+ 
+  );
+}
--- src/pages/contacts/contact-lists.tsx
+++ src/pages/contacts/contact-lists.tsx
@@ -2,13 +2,21 @@ import React, { MutableRefObject } from "react";
 import { useNavigate } from "react-router-dom";
 import { IContact } from "../../types";
 import SkeletonLoader from "../../components/skelton-loader";
+import EmptyData from "../../components/empty-data";
+import { Error } from "../../components/error";
 
 interface IcontactListsProps {
   items: IContact[];
   loaderRef: MutableRefObject<null>;
   loading: boolean;
+  error: boolean;
 }
-const ContactLists = ({ items, loaderRef, loading }: IcontactListsProps) => {
+const ContactLists = ({
+  items,
+  loaderRef,
+  loading,
+  error,
+}: IcontactListsProps) => {
   const navigate = useNavigate();
   const recentData = JSON.parse(localStorage.getItem("recentContacts")!);
 
@@ -24,39 +32,46 @@ const ContactLists = ({ items, loaderRef, loading }: IcontactListsProps) => {
     navigate(`/contact/${item.id}`);
   }
 
+  if (error) {
+    return <Error />;
+  }
+  if (items?.length === 0) {
+    return <EmptyData />;
+  }
+
   return (
     <>
       <h2>Contact list</h2>
-      <ul>
-        {items.map((item, index) => (
-          <li
-            onClick={() => handleClick(item)}
-            key={index}
-            className="contact-list"
-          >
-            <div className="info">
-              <img src={item.avatar} alt={item.first_name} width="50" />
-              <div>
-                <p>{item.first_name}</p>
-                <p>{item.address || "___"}</p>
+      {loading ? (
+        Array.from({ length: 4 }).map((_, index) => (
+          <SkeletonLoader key={index} />
+        ))
+      ) : (
+        <ul>
+          {items.map((item, index) => (
+            <li
+              onClick={() => handleClick(item)}
+              key={index}
+              className="contact-list"
+            >
+              <div className="info">
+                <img src={item.avatar} alt={item.first_name} width="50" />
+                <div>
+                  <p>{item.first_name}</p>
+                  <p>{item.address || "___"}</p>
+                </div>
               </div>
-            </div>
-            <div className="info">
-              <img
-                alt="phone"
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABWElEQVR4nO3WvUvDQBgG8NOCIE6isyAujuLXajdHZxcXwVV0EVz8B1ysqDh1c/bjf3BwcMqQhnueS0IgHVzclIonBwkeJe1guatoH3inHPlxyb0vJ8Qo/y4A5kieA7juqkOt9ZgzmOQFSV1VANacwQCavWClVP1Pwo2hwCSPhgIrpeo9DtaHUmrJGRwEwQSAl26U5I5wHZKXFvxGckv4SBRFCyQ7BfwgfIbfu+5IKVe9wUmSTJNsF3grDMNZbzjJTQCfBf6U5/mUNxzAsXW677TWNZ94w8Jv0zSdrFoXx/EyyQ0p5UpZSqnFH8Na63GSNxb+mGXZjL1GSrld9HvV1NsfBK+RPLVe1jI7NM9Mn1vtVzX1mmLQANgl+V62Gsn7Yshop7CJuRAAeO6HOYGt/75H8tUrXMYMFpInJNM+8JVwFfMFAKwDOABwVl4SzcUxjuN5Z/AovzZfnft2YfvGmT0AAAAASUVORK5CYII="
-              />
-              <p>{item.phone}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-
-      {loading
-        ? Array.from({ length: 4 }).map((_, index) => (
-            <SkeletonLoader key={index} />
-          ))
-        : null}
+              <div className="info">
+                <img
+                  alt="phone"
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABWElEQVR4nO3WvUvDQBgG8NOCIE6isyAujuLXajdHZxcXwVV0EVz8B1ysqDh1c/bjf3BwcMqQhnueS0IgHVzclIonBwkeJe1guatoH3inHPlxyb0vJ8Qo/y4A5kieA7juqkOt9ZgzmOQFSV1VANacwQCavWClVP1Pwo2hwCSPhgIrpeo9DtaHUmrJGRwEwQSAl26U5I5wHZKXFvxGckv4SBRFCyQ7BfwgfIbfu+5IKVe9wUmSTJNsF3grDMNZbzjJTQCfBf6U5/mUNxzAsXW677TWNZ94w8Jv0zSdrFoXx/EyyQ0p5UpZSqnFH8Na63GSNxb+mGXZjL1GSrld9HvV1NsfBK+RPLVe1jI7NM9Mn1vtVzX1mmLQANgl+V62Gsn7Yshop7CJuRAAeO6HOYGt/75H8tUrXMYMFpInJNM+8JVwFfMFAKwDOABwVl4SzcUxjuN5Z/AovzZfnft2YfvGmT0AAAAASUVORK5CYII="
+                />
+                <p>{item.phone}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
       <div ref={loaderRef} />
     </>
   );
