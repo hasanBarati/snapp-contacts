60cf47baa94eeb6f1c27676c4185f4fc839f74e5
hasanb69 <hasanbaratiasp@gmail.com>
2024-07-26 15:28:27 +0330

    Fix Webpack configuration and update ContactLists component for infinite scrolling - Updated Webpack config to ensure only `.env` is loaded by `dotenv-webpack`. - Removed unnecessary reference to `.env.example` from Webpack config. - Added logging and improved error handling in `ContactLists` component for better debugging of infinite scroll.

--- src/pages/contacts/contact-lists.tsx
+++ src/pages/contacts/contact-lists.tsx
@@ -35,43 +35,40 @@ const ContactLists = ({
   if (error) {
     return <Error />;
   }
-  if (items?.length === 0) {
-    return <EmptyData />;
-  }
 
   return (
     <>
       <h2>Contact list</h2>
-      {loading ? (
-        Array.from({ length: 4 }).map((_, index) => (
-          <SkeletonLoader key={index} />
-        ))
-      ) : (
-        <ul>
-          {items.map((item, index) => (
-            <li
-              onClick={() => handleClick(item)}
-              key={index}
-              className="contact-list"
-            >
-              <div className="info">
-                <img src={item.avatar} alt={item.first_name} width="50" />
-                <div>
-                  <p>{item.first_name}</p>
-                  <p>{item.address || "___"}</p>
-                </div>
+      <ul>
+        {items.map((item, index) => (
+          <li
+            onClick={() => handleClick(item)}
+            key={index}
+            className="contact-list"
+          >
+            <div className="info">
+              <img src={item.avatar} alt={item.first_name} width="50" />
+              <div>
+                <p>{item.first_name}</p>
+                <p>{item.address || "___"}</p>
               </div>
-              <div className="info">
-                <img
-                  alt="phone"
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABWElEQVR4nO3WvUvDQBgG8NOCIE6isyAujuLXajdHZxcXwVV0EVz8B1ysqDh1c/bjf3BwcMqQhnueS0IgHVzclIonBwkeJe1guatoH3inHPlxyb0vJ8Qo/y4A5kieA7juqkOt9ZgzmOQFSV1VANacwQCavWClVP1Pwo2hwCSPhgIrpeo9DtaHUmrJGRwEwQSAl26U5I5wHZKXFvxGckv4SBRFCyQ7BfwgfIbfu+5IKVe9wUmSTJNsF3grDMNZbzjJTQCfBf6U5/mUNxzAsXW677TWNZ94w8Jv0zSdrFoXx/EyyQ0p5UpZSqnFH8Na63GSNxb+mGXZjL1GSrld9HvV1NsfBK+RPLVe1jI7NM9Mn1vtVzX1mmLQANgl+V62Gsn7Yshop7CJuRAAeO6HOYGt/75H8tUrXMYMFpInJNM+8JVwFfMFAKwDOABwVl4SzcUxjuN5Z/AovzZfnft2YfvGmT0AAAAASUVORK5CYII="
-                />
-                <p>{item.phone}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+            </div>
+            <div className="info">
+              <img
+                alt="phone"
+                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABWElEQVR4nO3WvUvDQBgG8NOCIE6isyAujuLXajdHZxcXwVV0EVz8B1ysqDh1c/bjf3BwcMqQhnueS0IgHVzclIonBwkeJe1guatoH3inHPlxyb0vJ8Qo/y4A5kieA7juqkOt9ZgzmOQFSV1VANacwQCavWClVP1Pwo2hwCSPhgIrpeo9DtaHUmrJGRwEwQSAl26U5I5wHZKXFvxGckv4SBRFCyQ7BfwgfIbfu+5IKVe9wUmSTJNsF3grDMNZbzjJTQCfBf6U5/mUNxzAsXW677TWNZ94w8Jv0zSdrFoXx/EyyQ0p5UpZSqnFH8Na63GSNxb+mGXZjL1GSrld9HvV1NsfBK+RPLVe1jI7NM9Mn1vtVzX1mmLQANgl+V62Gsn7Yshop7CJuRAAeO6HOYGt/75H8tUrXMYMFpInJNM+8JVwFfMFAKwDOABwVl4SzcUxjuN5Z/AovzZfnft2YfvGmT0AAAAASUVORK5CYII="
+              />
+              <p>{item.phone}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {loading
+        ? Array.from({ length: 4 }).map((_, index) => (
+            <SkeletonLoader key={index} />
+          ))
+        : null}
+      {items.length === 0 ? <EmptyData /> : null}
       <div ref={loaderRef} />
     </>
   );
